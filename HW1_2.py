764f6c8adcce639ae2a5161403cef0b245b35033
RunzhongZhang123 <39126431+RunzhongZhang123@users.noreply.github.com>
2019-02-18 21:16:50 -0500

    Delete HW1_2.py

--- HW1_2.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkContext
-sc = SparkContext()
-data = sc.textFile("epa-http.txt").map(lambda x: (x.split(' ')[0], x.split(' ')[len(x.split(' ')) - 1]))
-#print(data.take(15))
-data = data.filter(lambda x: x[1] != '-')
-#print(data.take(15))
-data = data.reduceByKey(lambda x, y: int(x) + int(y))
-data = data.sortBy(lambda x: -int(x[1]))
-print(data.take(15))
