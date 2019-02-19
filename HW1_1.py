748b798c2414df60904e105d964f491f234da9f5
RunzhongZhang123 <39126431+RunzhongZhang123@users.noreply.github.com>
2019-02-18 21:16:28 -0500

    Delete HW1_1.py

--- HW1_1.py
+++
@@ -1,8 +0,0 @@
-from pyspark import SparkContext
-sc = SparkContext()
-data = sc.textFile("epa-http.txt").map(lambda x: (x.split(' ')[0], x.split(' ')[len(x.split(' ')) - 1]))
-#print(data.take(15))
-data = data.filter(lambda x: x[1] != '-')
-#print(data.take(15))
-data = data.reduceByKey(lambda x, y: int(x) + int(y))
-print(data.take(15))
